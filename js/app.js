6b767202e1e4ef7b13609a5c84fb99d96436b473
Hoodoo42 <create@hoodooverse.ca>
2022-07-07 19:28:25 -0600

    configured and ran debugger for the first time

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let username ="Hoodoo";
+let user_age = 33;
+let is_member = true ;
+
+if(username === "admin" || user_age >= 30 && is_member === true){
+    console.log("Welcome Back!");
+}else{
+    console.log("Welcome");
+}
